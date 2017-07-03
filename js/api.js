f1e70bb0fb49a2d9f0eb05505dc3ccbe95adaced
Vancir <vancirprince@gmail.com>
2017-07-03 23:36:18 +0800

    点击button读取蓝牙信息显示到list里

--- js/api.js
+++ js/api.js
@@ -1,15 +1,18 @@
 document.bluetooth = [
-  {id: 1, name: 'one'},
-  {id: 2, name: 'two'},
-  {id: 3, name: 'three'},
-  {id: 4, name: 'four'},
-  {id: 5, name: 'five'},
-  {id: 6, name: 'six'},
+  {id: 1, name: 'one-test'},
+  {id: 2, name: 'two-test'},
+  {id: 3, name: 'three-test'},
+  {id: 4, name: 'four-test'},
+  {id: 5, name: 'five-test'},
+  {id: 6, name: 'six-test'},
 ];
 function getBluetooths() {
   return document.bluetooth;
 }
 
+function currentBluetooth() {
+  return document.bluetooth[document.connectedBluetoothID - 1];
+}
 
 function connectBluetooth(id, callBack) {
   document.connectedBluetoothID = id;
@@ -17,7 +20,18 @@ function connectBluetooth(id, callBack) {
     return callBack(null);
 }
 
-function currentBluetooth() {
-  return document.bluetooth[document.connectedBluetoothID - 1];
+
+function sendBluetooth(message, callBack){
+
+  console.log(message);
+
+  if (callBack) {
+    return callBack(null);
+  }else{
+    console.log("send message failed");
+  }
 }
 
+function readBluetooth(callBack){
+  console.log("")
+}
